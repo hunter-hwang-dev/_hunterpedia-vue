410b4cd2402670fe4b812239097e21a766721e5c
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2024-11-02 07:54:18 +0900

    chore: tailwind css 시작하기 https://tailwindcss.com/docs/installation

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], //Add the paths to all of your template files in your tailwind.config.js file. 이라는데 이거 맞나?
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
